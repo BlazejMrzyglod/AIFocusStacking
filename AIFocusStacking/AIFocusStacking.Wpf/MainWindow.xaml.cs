63ccbaad752baf1fa530a64ea74e8f0691f6952e
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2023-10-06 17:41:57 +0200

    Chosen photos now get saved

--- AIFocusStacking/AIFocusStacking.Wpf/MainWindow.xaml.cs
+++ AIFocusStacking/AIFocusStacking.Wpf/MainWindow.xaml.cs
@@ -27,22 +27,39 @@ namespace AIFocusStacking.Wpf
             InitializeComponent();          
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //Funkcja umożliwająca wybranie zdjęć
+        private void ChooseImagesButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.Multiselect = true;
             if (fileDialog.ShowDialog() == true)
+                SavePhotos(fileDialog.FileNames);
                 foreach (var file in fileDialog.FileNames)
                     ImagesWrapPanel.Children.Add(new Image { Source = new BitmapImage(new Uri(file)), Height = 200, Width = 200 });
         }
 
-        private void ImageTest_Drop(object sender, DragEventArgs e)
+        //Funkcja odpowiadająca za dodawanie "przeciągniętych" zdjęć
+        private void ImagesWrapPanel_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                SavePhotos(files);
                 foreach (var file in files)
                     ImagesWrapPanel.Children.Add(new Image { Source = new BitmapImage(new Uri(file)), Height = 200, Width = 200 });
+            }
+        }
+
+        //Funkcja zapisująca zdjęcia do późniejszej przeróbki
+        //TODO: Przenieść do serwisów
+        private void SavePhotos(string[] photos)
+        {
+            int i = 0;
+            foreach (var photo in photos) 
+            {
+                Directory.CreateDirectory("images");
+                File.Copy(photo, "images\\photo" + i + ".jpg"); //TODO: Rozszerzenie powinno być zmienne
+                i++;
             }
         }
 
