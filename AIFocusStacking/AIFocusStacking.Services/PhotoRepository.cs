4b08d828ca7f2ffd060a976c377e449cc4a29e8a
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2023-10-10 14:03:06 +0200

    Implemented all the functions in PhotoRepository

--- AIFocusStacking/AIFocusStacking.Services/PhotoRepository.cs
+++ AIFocusStacking/AIFocusStacking.Services/PhotoRepository.cs
@@ -8,9 +8,22 @@ namespace AIFocusStacking.Services
 {
     public class PhotoRepository : IPhotoRepository
     {
+        protected string _repositoryFolder = "images";
         public ServiceResult Create(string photo)
         {
-            throw new NotImplementedException();
+            ServiceResult result = new ServiceResult();
+            try
+            {
+                Directory.CreateDirectory(_repositoryFolder);
+                File.Copy(photo, _repositoryFolder + "\\" + photo.Split("\\").Last());
+                result.Result = ServiceResultStatus.Succes;
+            }
+            catch (Exception e)
+            {
+                result.Result = ServiceResultStatus.Error;
+                result.Messages.Add(e.Message);
+            }
+            return result;
         }
 
         public ServiceResult CreateMultiple(string[] photos)
@@ -18,12 +31,10 @@ namespace AIFocusStacking.Services
             ServiceResult result = new ServiceResult();
             try
             {
-                int i = 0;
+                Directory.CreateDirectory(_repositoryFolder);
                 foreach (var photo in photos)
                 {
-                    Directory.CreateDirectory("images");
-                    File.Copy(photo, "images\\photo" + i + ".jpg"); //TODO: Rozszerzenie powinno być zmienne
-                    i++;
+                    File.Copy(photo, _repositoryFolder + "\\" + photo.Split("\\").Last());
                 }
                 result.Result = ServiceResultStatus.Succes;
             }
@@ -39,32 +50,106 @@ namespace AIFocusStacking.Services
 
         public ServiceResult Delete(string photo)
         {
-            throw new NotImplementedException();
+            ServiceResult result = new ServiceResult();
+            try
+            {
+                File.Delete(_repositoryFolder + "\\" + photo);
+                result.Result = ServiceResultStatus.Succes;
+            }
+            catch (Exception e)
+            {
+                result.Result = ServiceResultStatus.Error;
+                result.Messages.Add(e.Message);
+            }
+            return result;
         }
 
         public ServiceResult DeleteMultiple(string[] photos)
         {
-            throw new NotImplementedException();
+            ServiceResult result = new ServiceResult();
+            try
+            {
+                foreach (var photo in photos)
+                {
+                    File.Delete(_repositoryFolder + "\\" + photo);
+                    result.Result = ServiceResultStatus.Succes;
+                }          
+            }
+            catch (Exception e)
+            {
+                result.Result = ServiceResultStatus.Error;
+                result.Messages.Add(e.Message);
+            }
+            return result;
         }
 
         public ServiceResult Edit(string photo)
         {
-            throw new NotImplementedException();
+            ServiceResult result = new ServiceResult();
+            try
+            {
+                Directory.CreateDirectory(_repositoryFolder);
+                File.Delete(_repositoryFolder + "\\"+ photo);
+                File.Copy(photo, _repositoryFolder + "\\" + photo.Split("\\").Last());
+                result.Result = ServiceResultStatus.Succes;
+            }
+            catch (Exception e)
+            {
+                result.Result = ServiceResultStatus.Error;
+                result.Messages.Add(e.Message);
+            }
+            return result;
         }
 
         public ServiceResult EditMultiple(string[] photos)
         {
-            throw new NotImplementedException();
+            ServiceResult result = new ServiceResult();
+            try
+            {
+                foreach (var photo in photos)
+                {
+                    Directory.CreateDirectory(_repositoryFolder);
+                    File.Delete(_repositoryFolder + "\\" + photo);
+                    File.Copy(photo, _repositoryFolder + "\\" + photo.Split("\\").Last());
+                    result.Result = ServiceResultStatus.Succes;
+                }            
+            }
+            catch (Exception e)
+            {
+                result.Result = ServiceResultStatus.Error;
+                result.Messages.Add(e.Message);
+            }
+            return result;
         }
 
         public IEnumerable<string> GetAll()
         {
-            throw new NotImplementedException();
+            IEnumerable<string> photos = Directory.GetFiles(_repositoryFolder);
+            return photos;
         }
 
         public string GetSingle(string photo)
         {
-            throw new NotImplementedException();
+            return Directory.GetFiles(_repositoryFolder).Where(r => r == photo).SingleOrDefault();
+        }
+
+        public ServiceResult ChangeDirectory(string directory)
+        {
+            ServiceResult result = new ServiceResult();
+            try
+            {
+                _repositoryFolder = directory;
+                result.Result = ServiceResultStatus.Succes;
+            }
+            
+        
+            catch (Exception e)
+            {
+                result.Result = ServiceResultStatus.Error;
+                result.Messages.Add(e.Message);
+            }
+
+            return result;
         }
     }
 }
