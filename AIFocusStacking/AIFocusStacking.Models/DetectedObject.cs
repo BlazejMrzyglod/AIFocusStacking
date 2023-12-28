503646480600de1fe762ccae35f6cb70b8d0e728
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2023-12-29 00:06:49 +0100

    Added models

---
+++ AIFocusStacking/AIFocusStacking.Models/DetectedObject.cs
@@ -0,0 +1,12 @@
+﻿using OpenCvSharp;
+
+namespace AIFocusStacking.Models
+{
+	public class DetectedObject
+	{
+        public Rect? Box { get; set; }
+        public List<Point>? Mask { get; set; }
+        public int? Class { get; set; }
+        public int? Intensity { get; set; }
+    }
+}
---
+++ AIFocusStacking/AIFocusStacking.Models/Photo.cs
@@ -0,0 +1,14 @@
+﻿using OpenCvSharp;
+
+namespace AIFocusStacking.Models
+{
+	public class Photo
+	{
+        public Mat? Matrix { get; set; }
+        public Mat? MatrixAfterGauss { get; set; }
+        public Mat? MatrixAfterLaplace { get; set; }
+        public List<DetectedObject>? DetectedObjects { get; set; }
+        public string? Path { get; set; }
+		
+    }
+}
