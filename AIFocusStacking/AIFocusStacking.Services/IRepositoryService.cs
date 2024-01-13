58f178b5e23f635305f36d485f58075ee943e2ce
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-01-14 00:15:29 +0100

    Change interface to be generic

---
+++ AIFocusStacking/AIFocusStacking.Services/IRepositoryService.cs
@@ -0,0 +1,15 @@
+﻿namespace AIFocusStacking.Services
+{
+	//Interfejs repozytorium zdjęć
+	public interface IRepositoryService<T>
+	{
+		IEnumerable<T> GetAll();
+		IEnumerable<T> GetMultiple(string[] names);
+		T GetSingle(string name);
+		ServiceResult Add(string path);
+		ServiceResult AddMultiple(string[] paths);
+		ServiceResult Delete(string name);
+		ServiceResult DeleteMultiple(string[] names);
+		ServiceResult DeleteAll();
+	}
+}
